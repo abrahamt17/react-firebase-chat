252d3f86e81ff74c87ee6c7349d91344af92eafe
abraham <abrahamtikuye.com>
2024-06-18 03:32:02 +0300

    login

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -5,7 +5,27 @@ const Login=()=> {
     <div className='login'>
         <div className="item">
             <h2>welcomme back</h2>
+            <form action="">
+               <input type="text" placeholder='Email'  name='email'/>
+               <input type="password" placeholder='password' name='password' />
+               <button>Sing In</button> 
+            </form>
+            
         </div>
+        <div className="separator"></div>
+        <div className="item">
+      <h2>Create New Account</h2>
+             <form>
+            <label htmlFor="file">
+            <img src="" alt="" />
+                upload an image
+            </label>
+            <input type="file " id='file' style={{display:"none"}} />
+            <input type="text" placeholder='Email'  name='email'/>
+               <input type="password" placeholder='password' name='password' />
+               <button>Sing Up</button> 
+               </form>
+    </div>
     </div>
   )
 } 
